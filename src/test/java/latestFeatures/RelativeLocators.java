953c26f955529859df9c70d39ca71ba33f1e6d03
DESKTOP-6T8U07T\User <mohamed.hamdy.issa@gmail.com>
2022-08-10 14:32:15 +0200

    Relative locators with real time examples

--- src/test/java/latestFeatures/RelativeLocators.java
+++ src/test/java/latestFeatures/RelativeLocators.java
@@ -3,6 +3,7 @@ package latestFeatures;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WrapsDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.openqa.selenium.support.locators.RelativeLocator.*;
@@ -21,6 +22,19 @@ public class RelativeLocators {
 		String labelName = driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText();
 
 		System.out.println(labelName);
+
+		WebElement dateofBirth = driver.findElement(By.cssSelector("[for='dateofBirth']"));
+
+		driver.findElement(with(By.tagName("input")).below(dateofBirth)).click();
+
+		WebElement iceCreamlabel = driver.findElement(By.cssSelector("[for='exampleCheck1']"));
+
+		driver.findElement(with(By.tagName("input")).toLeftOf(iceCreamlabel)).click();
+
+		WebElement rdb = driver.findElement(By.id("inlineRadio1"));
+
+		String labelName2 = driver.findElement(with(By.tagName("label")).toRightOf(rdb)).getText();
+		System.out.println(labelName2);
 
 	}
 
